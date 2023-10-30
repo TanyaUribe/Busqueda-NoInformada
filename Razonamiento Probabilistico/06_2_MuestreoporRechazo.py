c8619befcffc5dc53ccd26cf858f287231cacb6b
Tanya Uribe <126315885+TanyaUribe@users.noreply.github.com>
2023-10-30 11:07:31 -0600

    Create 06_2_MuestreoporRechazo.py

---
+++ Razonamiento Probabilistico/06_2_MuestreoporRechazo.py
@@ -0,0 +1,23 @@
+
+import random
+
+# Función de densidad de probabilidad de una distribución (ejemplo)
+def pdf(x):
+    if x >= 0 and x <= 1:
+        return 2 * x
+    return 0
+
+# Función para realizar muestreo por rechazo
+def rejection_sampling(pdf, x_min, x_max):
+    while True:
+        x = random.uniform(x_min, x_max)
+        y = random.uniform(0, pdf(x_max))
+        
+        if y <= pdf(x):
+            return x
+
+# Realizar muestreo por rechazo
+x_min = 0
+x_max = 1
+sample = rejection_sampling(pdf, x_min, x_max)
+print("Muestreo por Rechazo:", sample)
